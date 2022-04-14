1e9a1ac3dc244063f6765fd116f7e9a631b01a75
Carol Altimas <carol.altimas@mail.mcgill.ca>
2022-04-14 11:35:57 -0400

    Updating Ta model.

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,4 +1,6 @@
 mongoose = require('mongoose');
+
+// SEMESTER
 const semesterSchema = new mongoose.Schema({
     term_month_year: Number,
     instructor_assigned_name: String, 
@@ -6,10 +8,16 @@ const semesterSchema = new mongoose.Schema({
 
 const Semester = mongoose.model('Semester', semesterSchema);
 
+// COURSE 
 const courseSchema = new mongoose.Schema({
     past_semesters: [Semester],
     course_num: Number, 
+    term_month_year: Number,
     course_name: String,
+    course_type: String,
+    TA_quota: Number,
+    instructor_name: String,
+    course_enrollment_num: Number,
     curr_tas: [Ta],
     curr_prof: [User],
     curr_sem: Semester,
@@ -17,18 +25,59 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model('Course', courseSchema);
 
-
-const student = new User({
-    first_name: 'Carol',
-    last_name: 'Altimas', 
-    email: 'carol.altimas@mail.mcgill.ca',
-    student_id: 09283920,
-    id: 023,
-    username: 'carolaltimas', 
-    password: 'bob', 
-    courses_reg: [],
-    access: [4]
+// USER 
+const userSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String, 
+    email: String,
+    id: Number, 
+    username: String, 
+    password: String, 
+    courses_reg: [Course],
+    access: [Number]
 });
+
+const User = mongoose.model('User', userSchema);
+
+// TA 
+
+const taSchema = new mongoose.Schema({
+    user_info: User,
+    TA_name: String,
+    grad_ugrad: String,
+    student_id: Number,
+    notes: [String],
+    term_month_year: Number, 
+    supervisor_name: String,
+    priority: String, // could be a boolean
+    hours: Number,
+    date_applied: Number,
+    location: String,
+    phone: Number,
+    degree: String,
+    courses_applied_for: [Course],
+    open_to_other_courses: String, //could be a boolean
+    assigned_responsibility: [Course],
+    rating_average : Number,
+
+});
+
+const Ta = mongoose.model('Ta', taSchema);
+
+const studentSchema = new mongoose.Schema({
+    user: User,
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+// PROFESSOR 
+
+// TA ADMIN 
+
+// SYS_OP
+
+
+// PERFORMANCE REPORTS
 
 const preportSchema = new mongoose.Schema({
     term_month_year: Number,
@@ -39,23 +88,3 @@ const preportSchema = new mongoose.Schema({
 });
 
 const PReport = mongoose.model('PReport', preportSchema);
-
-const taSchema = new mongoose.Schema({
-    user_info: User,
-    course_num: Number, 
-    TA_name: String,
-    comment: String,
-});
-
-const Ta = mongoose.model('Ta', taSchema);
-
-
-
-const studentSchema = new mongoose.Schema({
-    roll_no: Number,
-    name: String, 
-    year: Number,
-    subjects: [String],
-});
-
-const Student = mongoose.model('Student', studentSchema);

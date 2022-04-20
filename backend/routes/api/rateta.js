9fc9a3ac675834249a6163804bee3e6f3a273bd9
chantelCode <101425961+chantelCode@users.noreply.github.com>
2022-04-20 16:38:44 -0400

    Various pages for Vella to add

---
+++ backend/routes/api/rateta.js
@@ -0,0 +1,5 @@
+// The user specifies which course (course_type, course_num) and which term (term_month_year).
+// They choose a TA associated with that course (from the courses "tas" variable)
+// And are allowed to rate a TA from 0 to 5 (add to student_ratings)
+// And a textbox allows them to put in a short comment (add to student_comments)
+// Finally a submit button to process this review.

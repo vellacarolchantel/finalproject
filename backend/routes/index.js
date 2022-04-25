851749a285cd2e9f3fdd53af46c8678e801ae917
chantelCode <101425961+chantelCode@users.noreply.github.com>
2022-04-24 20:24:50 -0400

    Added my routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,30 @@
-var router = require('express').Router();
+const express = require("express");
+const server = express.Router();
 
-router.use(require('./api'));
+//app.use("/",router);
+server.use('ta_admin', require('./ta_admin'));
+server.use('ta_management', require('./ta_management'));
+server.use('importCSV', require('./importCSV'));
+server.use('login', require('./login'));
+server.use('manualprof', require('./manualprof'));
+server.use('rateta', require('./rateta'));
+server.use('register', require('./register'));
+server.use('usermanagement', require('./usermanagement'));
 
-module.exports = router;
+
+
+server.use(function(err, req, res, next){
+  if(err.name === 'ValidationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors, key){
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {})
+    });
+  }
+
+  return next(err);
+});
+
+module.exports = server;

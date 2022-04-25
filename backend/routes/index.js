bdd47ae1862b8bf5b671da4e1228bc5295b55648
chantelCode <101425961+chantelCode@users.noreply.github.com>
2022-04-25 17:32:51 -0400

    Debugged so server starts

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const server = express.Router();
 
 //app.use("/",router);
-server.use('ta_admin', require('./ta_admin'));
-server.use('ta_management', require('./ta_management'));
-server.use('importCSV', require('./importCSV'));
-server.use('login', require('./login'));
-server.use('manualprof', require('./manualprof'));
-server.use('rateta', require('./rateta'));
-server.use('register', require('./register'));
-server.use('usermanagement', require('./usermanagement'));
+server.use('ta_admin', require('./api/ta_admin'));
+server.use('ta_management', require('./api/ta_management'));
+server.use('importCSV', require('./api/importCSV'));
+server.use('login', require('./api/login'));
+server.use('manualprof', require('./api/manualprof'));
+server.use('rateta', require('./api/rateta'));
+server.use('register', require('./api/register'));
+server.use('usermanagement', require('./api/usermanagement'));
 
 
 

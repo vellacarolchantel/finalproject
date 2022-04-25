26f5f1a3bd72125395ebc0d9158577809f7dba4f
chantelCode <101425961+chantelCode@users.noreply.github.com>
2022-04-24 20:16:53 -0400

    Added the hashing functions to User
    Can be used to hash the password/ID as well as compare if a user-submitted password is equivalent to the hashed password during login.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,6 @@
 mongoose = require('mongoose');
+bcrypt = require('bcrypt'),
+SALT_WORK_FACTOR = 7 ;
 
 const userSchema = new mongoose.Schema({
     first_name: String,
@@ -11,6 +13,47 @@ const userSchema = new mongoose.Schema({
     courses_reg: [Course],
     access: [Number]
 });
+
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.username, salt, function (err, hash) {
+            if (err) return next(err);
+
+            // override using hashed user
+            if (user.id == user.username) {
+            user.id = hash;
+        }
+        });
+    });
+
+    // only hash the password at registration or when modified
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            // override using hashed password
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// compares the hashed submitted password with the hashed database password
+userSchema.methods.comparePassword = function (submittedPassword, cb) {
+    bcrypt.compare(submittedPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+}
 
 const User = mongoose.model('User', userSchema);
 

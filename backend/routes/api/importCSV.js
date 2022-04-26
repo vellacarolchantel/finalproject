dbfcc5db3dcb9873dcc1e37e8a6e7f774329c699
chantelCode <101425961+chantelCode@users.noreply.github.com>
2022-04-26 16:24:01 -0400

    Update importCSV.js

--- backend/routes/api/importCSV.js
+++ backend/routes/api/importCSV.js
@@ -1,41 +1,34 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
-const User = require("../../models/User.js");
-const Professor = require("../../models/Professor.js");
-const mongodb = require("mongodb").MongoClient;
-const csvtojson = require("csvtojson");
-
-app.use(bodyParser.urlencoded({ extended: false }));
+/* needs to be refactored to connect to the frontend
 
 
-app.get('/importCSV', function (req, res) {
-    res.sendFile(__dirname + '/importCSV.html');
-});
-
-app.post('/importCSV', (req, res) => {
-    csvtojson()
-        .fromFile("TACohort.csv")
-        .then(csvData => {
-            console.log(csvData);
-            mongodb.connect(
-                process.env.MONGODB_URI,
-                { useNewUrlParser: true, useUnifiedTopology: true },
-                (err, client) => {
-                    if (err) throw err;
-                    client
-                        .db("FINALPROJECT")
-                        .collection("tas")
-                        .insertMany(csvData, (err, res) => {
-                            if (err) throw err;
-                            console.log(`Inserted: ${res.insertedCount} TAs`);
-                            client.close();
-                        }, { ordered: true });
-                }
-            );
+        app.get('/importCSV', function (req, res) {
+            res.sendFile(__dirname + '/manualtesting/importCSV.html');
         });
 
-});
+        app.post('/importCSV', (req, res) => {
+            csvtojson()
+                .fromFile("testcsv.csv")
+                .then(csvData => {
+                    console.log(csvData);
+                    mongodb.connect(
+                        process.env.MONGODB_URI,
+                        { useNewUrlParser: true, useUnifiedTopology: true },
+                        (err, client) => {
+                            if (err) throw err;
+                            client
+                                .db("FINALPROJECT")
+                                .collection("courses")
+                                .insertMany(csvData, (err, res) => {
+                                    if (err) throw err;
+                                    console.log(`Inserted: ${res.insertedCount} Courses`);
+                                    client.close();
 
+                                }, { ordered: true });
+                        }
+                    );
+                })
+            res.sendFile(__dirname + '/manualtesting/importCSV.html')
+
+        });
+        
+        */

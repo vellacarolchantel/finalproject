9cc0a67e69033835f7f4552e28499e406ba26c91
chantelCode <101425961+chantelCode@users.noreply.github.com>
2022-04-26 16:26:35 -0400

    Update importprof.js

--- backend/routes/api/importprof.js
+++ backend/routes/api/importprof.js
@@ -1,6 +1,45 @@
-/*
- 
-For now just an import CSV button and a submit button.
-Maybe a separate tab if they want to import a CSV for Professors or Courses?
+/* needs to be refactored to connect to the frontend
 
- */
+app.get('/manualprof', function (req, res) {
+            res.sendFile(__dirname + '/manualtesting/manualprof.html');
+        });
+
+        app.post('/manualprof', (req, res) => {
+
+            const names = req.body.full_name.split(" ");
+            console.log(names[0]);
+            console.log(names[1]);
+            var userID = req.body.full_name;
+            
+            var newProf = new Professor({
+                ratings_given: req.body.ratings_given,
+                curr_courses: req.body.curr_courses.split(" "),
+                name: req.body.full_name,
+                id: userID,
+            });
+
+
+            User.findOne({ first_name: names[0], last_name: names[1] }, function (err, profUser) {
+                if (err) {
+                    console.log(err);
+                 }
+                else {
+                    console.log("Result : " + profUser.id);
+                    newProf.id = profUser.id;
+                }
+            });
+
+
+            newProf.save(function (err) {
+                if (err) throw err;
+                newProf.save().then(() => console.log("Successfully added Prof!"));
+                res.sendFile(__dirname + '/manualtesting/dashboard.html');
+
+            });
+
+
+
+        });
+
+       
+*/
